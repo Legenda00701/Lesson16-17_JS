2233d8f0b035c095b68cfcf828affa000cf7fcd3
Legenda00701 <deldla917@gmail.com>
2024-09-02 17:58:23 +0600

    -

---
+++ Lesson16-17_JS.js
@@ -0,0 +1,78 @@
+//1
+
+// let name = prompt("Как вас зовут?");
+// let age = prompt("Сколько вам лет?")
+
+// alert("Ваше имя: " + name);
+// alert("Вам " + age + " лет");
+
+
+//2
+
+// let num1 = prompt("Первое число:");
+// let num2 = prompt("Второе число для деления:");
+
+// let sum = num1 / num2;
+// console.log(sum);
+
+
+//3
+
+// let notifications = confirm("Вы хотите получать уведомления? ")
+// console.log(notifications)
+
+
+//4
+
+// let age1 = prompt("Укажите ваш возраст");
+// let answered = age1 !== null && age1 !== "";
+// console.log(answered);
+
+
+//5
+
+// let num3 = prompt("Укажите число:");
+// let cube = num3 * num3 * num3;
+// alert(cube)
+
+
+//6
+
+// let str1 = prompt("Начало");
+// let str2 = prompt("Конец");
+// alert(str1 + " " + str2)
+
+
+//7
+
+// let num1 = parseFloat(prompt("Введите первое число:"));
+// let num2 = parseFloat(prompt("Введите второе число:"));
+// let num3 = parseFloat(prompt("Введите третье число:"));
+
+// let average = (num1 + num2 + num3) / 3;
+// alert("Среднее значение: " + average);
+
+
+//8
+
+// let hours = parseInt(prompt("Введите количество часов:"));
+// let minutes = parseInt(prompt("Введите количество минут:"));
+// let seconds = parseInt(prompt("Введите количество секунд:"));
+
+// let totalSeconds = (hours * 3600) + (minutes * 60) + seconds;
+// alert("Общее количество секунд: " + totalSeconds);
+
+
+//9
+
+// let celsia2 = prompt("Градусы в Цельсиях: ");
+// let fahrenheit2 = (celsia2 * 9/5) + 32;
+// let kelvin2 = celsia2 + 273.15;
+// alert("Температура в градусах Фаренгейта: " + fahrenheit2);
+// alert("Температура в градусах Кельвина: " + kelvin2);
+
+
+//10
+
+let radius = prompt("Радиус для круга: ");
+alert("Площадь круга: " + radius * radius);
